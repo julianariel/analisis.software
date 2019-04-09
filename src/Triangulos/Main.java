2f00b328ed864d2f32367f963ea239b1ffd6a78e
Tomás Handley <tomashandley@hotmail.com>
2019-04-09 09:00:53 -0300

    -Clase Triangulo creada y validaciones de tipo de triangulo

--- src/Triangulos/Main.java
+++ src/Triangulos/Main.java
@@ -4,24 +4,40 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int l1, l2, l3 = 0;
-
-		System.out.println("Ingrese el primer lado:\n");
-		l1 = in.nextInt();
-
-		System.out.println("Ingrese el segundo lado:\n");
-		l2 = in.nextInt();
-
-		System.out.println("Ingrese el tercer lado:\n");
-		l3 = in.nextInt();
-
-		System.out.println(l1);
-		System.out.println(l2);
-		System.out.println(l3);
+public static void main(String[] args) throws Exception {
 		
-		System.out.println("Probando commit");
+		Triangulo triangulo = new Triangulo();
+		
+		System.out.println("Ingrese el lado 1: ");
+		Scanner sc = new Scanner(System.in);
+		triangulo.setL1(sc.nextInt());
+		
+		System.out.println("Ingrese el lado 2: ");
+		sc = new Scanner(System.in);
+		triangulo.setL2(sc.nextInt());
+		
+		System.out.println("Ingrese el lado 3: ");
+		sc = new Scanner(System.in);
+		triangulo.setL3(sc.nextInt());
+
+		System.out.println(triangulo.getL1() + ", " + triangulo.getL2() + ", " + triangulo.getL3());
+		
+		if(!triangulo.esTriangulo()) {
+			throw new Exception("No es posible armar el triangulo con esos lados.");
+		} else {
+			System.out.println("Triángulo válido");
+		}
+		
+		if(triangulo.esEquilatero()) {
+			System.out.println("El triángulo es equilatero.");
+		} else {
+			if(triangulo.esEscaleno()) {
+				System.out.println("El triángulo es escaleno.");
+			} else {
+				System.out.println("El triángulo es isóceles.");
+			}
+		}
+		
 	}
 
 }
---
+++ src/Triangulos/Triangulo.java
@@ -0,0 +1,61 @@
+package Triangulos;
+
+public class Triangulo {
+
+	private Integer l1;
+	private Integer l2;
+	private Integer l3;
+	
+	public boolean esTriangulo() {
+		
+		if( l1 <= 0 || l2 <= 0 || l3 <= 0) {
+			return false;
+		}
+		
+		if (l1 + l2 <= l3 || l1 + l3 <= l2 || l2 + l3 <= l1) {
+			return false; 
+		} 
+		return true; 
+	}
+	
+	public boolean esEscaleno() {
+		if(l1 != l2 && l1 != l3 && l3 != l2) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean esEquilatero() {
+		if(l1 == l2 && l1 == l3) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean esIsoceles() {
+		if(l1 == l2 && l1 != l3) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Integer getL1() {
+		return l1;
+	}
+	public void setL1(Integer l1) {
+		this.l1 = l1;
+	}
+	public Integer getL2() {
+		return l2;
+	}
+	public void setL2(Integer l2) {
+		this.l2 = l2;
+	}
+	public Integer getL3() {
+		return l3;
+	}
+	public void setL3(Integer l3) {
+		this.l3 = l3;
+	}
+	
+}
